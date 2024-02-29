dc786586b73331470898d7e9f7377c01feafd3b4
lunacles <lunahackley@gmail.com>
2024-02-29 17:10:30 -0600

    Create colors.ts

---
+++ public/colors.ts
@@ -0,0 +1,23 @@
+import Color from './color.js'
+
+namespace Colors {
+  export const pureBlack = new Color('#000000')
+  export const pureWhite = new Color('#ffffff')
+  export const white = new Color('#f6f6f6')
+  export const black = new Color('#0e0e0e')
+  export const bgBlack = new Color('#202027')
+  export const lightBlue = new Color('#b2dee6')
+  export const burple = new Color('#5f66c4')
+  export const navyBlue = new Color('#434879')
+  export const emperor = new Color('#4e4447')
+  export const darkGray = new Color('#333747')
+  export const gray = new Color('#b2b2b2')
+  export const red = new Color('#de7076')
+
+  export const googleRed = new Color('#ea4335')
+  export const googleYellow = new Color('#fbbc04')
+  export const googleGreen = new Color('#34a853')
+  export const googleBlue = new Color('#4285f4')
+}
+
+export default Colors
