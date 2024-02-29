00308727eeda6043c381b10e12b733d19f98acff
lunacles <lunahackley@gmail.com>
2024-02-29 17:10:40 -0600

    Create document.ts

---
+++ public/document.ts
@@ -0,0 +1,69 @@
+import { c } from './canvas.js'
+import Global from './global.js'
+import Interaction from './interaction.js'
+
+Interaction.settings.get('mouse').set('preventDefault', true)
+Interaction.settings.get('mouse').set('dispatchAfterRelease', true)
+Interaction.settings.get('mouse').set('scrollSpeed', 15)
+
+if (!Global.mobile) {
+  Interaction.editEvent('mousedown').bind('default')
+  Interaction.editEvent('mouseup').bind('default')
+  Interaction.editEvent('mousemove').bind('default')
+  Interaction.editEvent('wheel').bind('default')
+  Interaction.editEvent('contextmenu').bind('default')
+} else {
+  Interaction.editEvent('touchstart').bind('default')
+  Interaction.editEvent('touchcancel').bind('default')
+  Interaction.editEvent('touchend').bind('default')
+  Interaction.editEvent('touchmove').bind('default')
+}
+Interaction.editEvent('keydown').bind('default')
+
+const Document = {
+  get width() {
+    return window.innerWidth
+  },
+  get height() {
+    return window.innerHeight
+  },
+  get centerX() {
+    return Document.width * 0.5
+  },
+  get centerY() {
+    return Document.height * 0.5
+  },
+  get ratio() {
+    return window.devicePixelRatio
+  },
+  canvas: c,
+  holdTime: 0,
+  refreshCanvas(timeDelta: number): void {
+    c.setSize({ width: window.innerWidth, height: window.innerHeight, scale: window.devicePixelRatio })
+    c.setViewport({ x: 0, y: 0, width: c.width, height: c.height })
+
+    // TODO: rewrite this in a better way. I felt lazy and cut corners
+    if (Interaction.mouse.leftHeld && !Interaction.mouse.moving) {
+      Document.holdTime++
+      if (Document.holdTime > 30) {
+        Interaction.mouse.scroll = 0
+        Interaction.mouse.targetScroll = 0
+      }
+    } else {
+      Document.holdTime = 0
+    }
+
+    if (Document.holdTime <= 30) {
+      let smoothFix: number = Interaction.mouse.leftHeld ? 1 : 0.075 * (timeDelta / 16.67)
+      Interaction.mouse.scroll += (Interaction.mouse.targetScroll - Interaction.mouse.scroll) * smoothFix
+      Interaction.mouse.targetScroll -= Interaction.mouse.targetScroll * smoothFix
+    }
+
+    if (Interaction.mouse.left)
+      console.log('a')
+
+    Interaction.reset()
+  },
+}
+
+export default Document
