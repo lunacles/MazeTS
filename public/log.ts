d4fec99aa8fe0fadbc8254a07766f9c6d84557df
lunacles <lunahackley@gmail.com>
2024-02-29 21:52:52 -0600

    Make Log non-namespace

--- public/log.ts
+++ public/log.ts
@@ -1,17 +1,17 @@
-namespace Log {
-  const startTime: number = Date.now()
-  const time = (): string => {
-    return `[${new Date().toISOString()}] [${((Date.now() - startTime) * 0.001).toFixed(3)}]`
-  }
-  export const error = (reason: string, err: Error): void => {
-    console.error(time(), 'ERROR:', reason, err)
-  }
-  export const warn = (reason: string): void => {
-    console.warn(time(), 'WARN:', ...reason)
-  }
-  export const info = (info: string): void => {
-    console.info(time(), 'INFO:', info)
-  }
+const Log = {
+  startTime: Date.now(),
+  time(): string {
+    return `[${new Date().toISOString()}] [${((Date.now() - Log.startTime) * 0.001).toFixed(3)}]`
+  },
+  error(reason: string, err: Error): void {
+    console.error(Log.time(), 'ERROR:', reason, err)
+  },
+  warn(reason: string): void {
+    console.warn(Log.time(), 'WARN:', ...reason)
+  },
+  info(info: string): void {
+    console.info(Log.time(), 'INFO:', info)
+  },
 }
 
 export default Log
