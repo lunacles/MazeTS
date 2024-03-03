51f21f4a87081d4a79d4adfb2072863d96687436
lunacles <lunahackley@gmail.com>
2024-03-03 17:28:00 -0600

    Add maze test

--- index.ts
+++ index.ts
@@ -6,8 +6,28 @@ import {
   Circle,
   Bar,
 } from './public/elements.js'
+import MazeWall from './public/components/mazewall.js'
 
 import Colors from './public/colors.js'
+import Color from './public/color.js'
+
+import { Maze } from './public/src/maze.js'
+import {
+  RandomWalker
+} from './public/src/algorithms/random-walker.js'
+import { PRNG } from './public/src/random/prng.js'
+import { Hash } from './public/src/random/hash.js'
+
+let maze = new Maze({
+  width: 32,
+  height: 32,
+  prng: PRNG.simple(Hash.cyrb53('lol'))
+}).runAlgorithm(new RandomWalker({
+  seedAmount: 75,
+  turnChance: 0.2,
+  straightChance: 0.6,
+}))
+console.log(maze.array)
 
 let time = 0
 let tick = 0
@@ -39,6 +59,15 @@ let appLoop = async (newTime) => {
     x: spacing * 4 + rectSize * 2 + circleRadius * 2, y: spacing,
     width: rectSize * 1.5, height: rectSize,
   }).fill(Colors.white)
+
+  MazeWall.draw({
+    x: Document.centerX, y: Document.centerY,
+    width: 50, height: 100,
+  }).both(Colors.wall, Color.blend(Colors.wall, Colors.black, 0.2), 2)
+  MazeWall.draw({
+    x: Document.centerX + 50, y: Document.centerY,
+    width: 50, height: 50,
+  }).both(Colors.wall, Color.blend(Colors.wall, Colors.black, 0.2), 2)
 
   Document.refreshCanvas(timeElapsed)
 
