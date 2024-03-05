3b799c984aea3df4b343fcca098dbcd689d4ed15
lunacles <lunahackley@gmail.com>
2024-03-05 17:24:50 -0600

    Add walker config

--- public/src/algorithms/random-walker.ts
+++ public/src/algorithms/random-walker.ts
@@ -6,7 +6,6 @@ import {
 } from '../random/random.js'
 import {
   Walker,
-  MovementOptions,
 } from '../walker.js'
 
 type Coordinate = {
@@ -14,15 +13,10 @@ type Coordinate = {
   y: number,
 }
 
-interface Config {
-  seedAmount: number
-  turnChance: number
-  straightChance: number
-}
-
 export interface RandomWalkerInterface {
   maze: MazeInterface
   seedAmount: number
+  type: number
   turnChance: number
   straightChance: number
   ran: RandomInterface
@@ -32,17 +26,19 @@ export interface RandomWalkerInterface {
 export const RandomWalker = class RandomWalkerInterface {
   public maze: MazeInterface
   public seedAmount: number
+  public type: number
   public turnChance: number
   public straightChance: number
 
   public ran: RandomInterface
   private seeds: Array<Coordinate>
-  constructor({ seedAmount = 1, turnChance, straightChance }: Config) {
+  constructor(seedAmount: number = 1, inverse: boolean) {
     this.maze = null
     this.ran = null
     this.seedAmount = seedAmount
-    this.turnChance = turnChance
-    this.straightChance = straightChance
+    this.turnChance = 0
+    this.straightChance = 0
+    this.type = +inverse
 
     this.seeds = []
   }
@@ -57,32 +53,17 @@ export const RandomWalker = class RandomWalkerInterface {
           maze: this.maze,
           ran: this.ran,
         },
-        chances: {
-          straightChance: this.straightChance,
-          turnChance: this.turnChance,
-          branchChance: 0,
-        },
-        instructions: {
-          startDirection: [...MovementOptions.vertical, ...MovementOptions.horizontal],
-          branchDirections: [...MovementOptions.vertical, ...MovementOptions.horizontal],
-        },
-        settings: {
-          borderWrapping: false,
-          terminateOnContact: false,
-        },
-        limits: {
-          maxLength: Infinity,
-          maxTurns: Infinity,
-          maxBranches: Infinity,
-        }
+        chances: this.maze.walkerChances,
+        instructions: this.maze.walkerInstructions,
+        settings: this.maze.walkerSettings,
+        limits: this.maze.walkerLimits
       })
       for (let { x, y } of walker.walk()) {
-        this.maze.set(x, y, +!this.maze.inverse)
+        this.maze.set(x, y, this.type)
       }
     }
   }
   private validateCell(position: Coordinate): boolean {
-    //if (this.map.get(position.x, position.y) === (this.type + 0) % 2) return false
     if (!this.maze.has(position.x, position.y)) return false
     return true
   }
@@ -96,41 +77,10 @@ export const RandomWalker = class RandomWalkerInterface {
 
       if (this.validateCell(loc)) {
         this.seeds.push(loc)
-        this.maze.set(loc.x, loc.y, +!this.maze.inverse)
+        this.maze.set(loc.x, loc.y, this.type)
         amount++
         if (amount >= this.seedAmount) break
       }
     }
   }
-  /*
-  private walk(): void {
-    let perpendicular = ([x, y]: Pair): Array<Pair> => [[y, -x], [-y, x]]
-    for (let seed of this.seeds) {
-      let dir = this.maze.randomDirection([
-        Direction.Left,
-        Direction.Right,
-        Direction.Up,
-        Direction.Down,
-      ])
-      for (let i = 0; i < 1e3; i++) {
-        let [x, y] = dir
-        if (this.ran.float() <= this.straightChance) {
-          seed.x += x
-          seed.y += y
-        } else if (this.ran.float() <= this.turnChance) {
-          let [xx, yy] = this.ran.fromArray(perpendicular(dir))
-          seed.x += xx
-          seed.y += yy
-        } else {
-          break
-        }
-        if (this.validateCell(seed)) {
-          this.maze.set(seed.x, seed.y, +!this.maze.inverse)
-        } else {
-          break
-        }
-      }
-    }
-  }
-  */
 }
