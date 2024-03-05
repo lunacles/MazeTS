1768b67cc562b079c68d2709d7d30af9d8b96d4f
lunacles <lunahackley@gmail.com>
2024-03-05 17:23:50 -0600

    Add Directions to Global

--- public/global.ts
+++ public/global.ts
@@ -1,3 +1,40 @@
+interface Direction {
+  none: number
+  left: number
+  right: number
+  up: number
+  down: number
+}
+
+interface DiagonalDirection {
+  upLeft: number
+  downLeft: number
+  upRight: number
+  downRight: number
+}
+
+const Direction: Direction = {
+  none: 0,  // 0000
+  left: 1,  // 0001
+  right: 2, // 0010
+  up: 4,    // 0100
+  down: 8,  // 1000
+}
+
+const DiagonalDirections: DiagonalDirection = {
+  upLeft: Direction.left | Direction.up,
+  downLeft: Direction.left | Direction.down,
+  upRight: Direction.right | Direction.up,
+  downRight: Direction.right | Direction.down
+}
+
+interface MovementOptions {
+  all: Array<number> | number,
+  diagonal: Array<number> | number,
+  vertical: Array<number> | number,
+  horizontal: Array<number> | number,
+}
+
 interface FontConfig {
   family: string,
   style: string,
@@ -12,6 +49,9 @@ interface Global {
   fontConfig: FontConfig
   font: string
   fontFromSize: Function
+  direction: Direction
+  diagonalDirection: DiagonalDirection
+  movementOptions: MovementOptions
 }
 
 const global: Global = {
@@ -26,6 +66,26 @@ const global: Global = {
   },
   font: '',
   fontFromSize: (size: number): string => `${global.fontConfig.style} ${size}px ${global.fontConfig.family}`,
+  direction: Direction,
+  diagonalDirection: DiagonalDirections,
+  movementOptions: {
+    all: [
+      Direction.left, Direction.right,
+      Direction.up, Direction.down,
+      DiagonalDirections.upLeft, DiagonalDirections.upRight,
+      DiagonalDirections.downLeft, DiagonalDirections.downRight,
+    ],
+    diagonal: [
+      DiagonalDirections.upLeft, DiagonalDirections.upRight,
+      DiagonalDirections.downLeft, DiagonalDirections.downRight,
+    ],
+    vertical: [
+      Direction.up, Direction.down
+    ],
+    horizontal: [
+      Direction.left, Direction.right
+    ],
+  }
 }
 global.font = global.fontFromSize(global.fontConfig.size)
 
